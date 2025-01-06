849522709f28efdb585ad1b3db56104d6e960635
macie <maciekjanowski3@gmail.com>
2025-01-06 14:32:58 +0100

    commit 2

--- src/UrlInOut.java
+++ src/UrlInOut.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.*;
 
 public class UrlInOut {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://ewib.nbp.pl/plewibnra?dokNazwa=plewibnra.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the first three digits of bank account: ");
+        String accountNumber = bufferedReader.readLine();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String line;
+            System.out.println("Reads data from a file");
+
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\t+");
+                String bankNumber = words[0].trim();
+                String bankName = words[1].trim();
+
+                if (bankNumber.equals(accountNumber)) {
+                    System.out.println("Bank number : " + bankNumber + "Your bank name is : " + bankName);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error");
+            e.printStackTrace();
+        }
+    }
 }
